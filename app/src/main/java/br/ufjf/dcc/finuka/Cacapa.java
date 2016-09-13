bfe2ef05cfcef8accf562009c6b8bc09776d9560
Igor Knop <igorknop@gmail.com>
2016-09-13 15:28:16 -0300

    Move classes para pacotes mais descritivos.

---
+++ app/src/main/java/br/ufjf/dcc/finuka/Cacapa.java
@@ -0,0 +1,43 @@
+package br.ufjf.dcc.finuka;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by LucasRezende on 10/08/2016.
+ */
+public class Cacapa extends Objeto {
+
+    private  float raio;
+
+    public Cacapa() {
+    }
+
+    public Cacapa(float x, float y, float raio) {
+        super(x, y,0,0);
+        this.raio = raio;
+    }
+
+    public void  draw(Canvas canvas, Paint paint){
+            paint.setColor(Color.BLACK);
+
+            canvas.drawCircle(getX(),getY(),raio,paint);
+    }
+
+    public boolean collidecacapa(Bola b){
+        if(Math.sqrt(Math.pow(b.getX()- getX(),2)+Math.pow(b.getY()- getY(),2)) <= raio)
+            return true;
+        else
+            return false;
+
+    }
+
+    public float getRaio() {
+        return raio;
+    }
+
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+}
---
+++ app/src/main/java/br/ufjf/dcc/finuka/activities/MainActivity.java
@@ -0,0 +1,27 @@
+package br.ufjf.dcc.finuka.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.ufjf.dcc.finuka.views.GameView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GameView game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        game = new GameView(this);
+        game.setBackgroundColor(getResources().getColor(android.R.color.white));
+        setContentView(game);
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        game.release();
+    }
+}
